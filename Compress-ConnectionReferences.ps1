5dc8fa3fe2e5d96223b070c4cb3d3de8a3cf9db4
Tyler Hogsett <tyhogset@microsoft.com>
2021-02-10 13:11:15 -0800

    created script to compress connection referecnes

---
+++ Compress-ConnectionReferences.ps1
@@ -0,0 +1,4 @@
+& $PSScriptRoot\powershell\solutions\Compress-Solution.ps1 `
+    -SourceFolderPath $PSScriptRoot\ConnectionReferences\solution `
+    -SolutionZipFilePath $PSScriptRoot\ConnectionReferences_managed.zip `
+    -PackageType Managed
