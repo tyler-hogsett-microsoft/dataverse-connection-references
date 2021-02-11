8f2045eb231daf1506c02fc75d8d15c1bf434934
Tyler Hogsett <tyhogset@microsoft.com>
2021-02-10 23:04:01 -0800

    output psmodule path

--- powershell/environment-setup/Add-ModulesPath.ps1
+++ powershell/environment-setup/Add-ModulesPath.ps1
@@ -3,4 +3,5 @@ $modulesPath = Resolve-Path $PSScriptRoot\..\modules
 if(-not $env:PSModulePath.Contains($modulesPath))
 {
   $env:PSModulePath = "$env:PSModulePath;$modulesPath"
+  Write-Host $env:PSModulePath
 }
