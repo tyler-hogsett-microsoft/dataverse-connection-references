54ff3d894d80ec4a2e2d147d388a39617033370b
Tyler Hogsett <tyhogset@microsoft.com>
2021-02-10 20:18:47 -0800

    created plugins proj

---
+++ Plugins/Contoso.Plugins/AllWidgetsAreFoos.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Contoso.Plugins
+{
+    public class AllWidgetsAreFoos : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var target = (Entity)context.InputParameters["Target"];
+            target["contoso_name"] = "Foo";
+        }
+    }
+}
